0018264a54b2860db63161da0d0e969507955891
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-06-14 06:23:14 +0530

    Add files via upload

---
+++ Astroid2.py
@@ -0,0 +1,43 @@
+import pygame, random, sys
+
+
+pygame.init()
+clock=pygame.time.Clock()
+screen = pygame.display.set_mode((400,600))
+pygame.display.set_caption("Astroid")
+background_image = pygame.image.load("bg2.jpg").convert()
+
+
+#Color or rectangle
+BLUE=(0,0,255)
+player=pygame.Rect(200,200,30,30)
+
+
+WHITE=(255,255,255)
+enemy=pygame.Rect(100,100,30,30)
+xvel=2
+yvel=3
+
+while True:
+  screen.blit(background_image,[0,0])
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      
+  
+  enemy.x=enemy.x + xvel
+  enemy.y=enemy.y + yvel 
+  
+  if enemy.x < -250 or enemy.x > 650 :
+    xvel = -1*xvel
+  
+  if enemy.y < -250 or enemy.y > 850:  
+    yvel = -1*yvel
+
+  pygame.draw.rect(screen,BLUE,player)
+  pygame.draw.rect(screen,WHITE,enemy)
+
+  pygame.display.update()
+  clock.tick(30)
+  
+  
